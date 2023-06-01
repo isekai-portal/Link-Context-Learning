5810f705e86d855beb4f06933ca188744fa11220
chenkq <sy2106101@buaa.edu.cn>
2023-06-01 19:29:12 +0800

    add flickr check code to visualize

--- evaluate/flickr_check.py
+++ evaluate/flickr_check.py
@@ -1,26 +1,28 @@
 import json
 from pprint import pprint
-from typing import Optional
 
 import matplotlib.pyplot as plt
 
-from mllm.dataset import FlickrDataset, PlainBoxFormatter, de_norm_box_xyxy
+import sys
+import pathlib
+
+# sys path append the project dir to enable import from mllm
+sys.path.append(f"{pathlib.Path(__file__).parent.parent}")
+
+
 from mllm.utils import show, draw_bounding_boxes
+from mllm.dataset import DATASETS, PlainBoxFormatter, de_norm_box_xyxy, expand2square
 
 
 def format_examples(
-        filename: str,
-        prediction_file: str,
-        image_folder=None,
-        template_string: str = "Please specify the location of <expr> using the bounding box's top-left and bottom-right coordinates. Make sure the coordinates are normalized between 0 and 1.",
-        template_file: Optional[str] = None,
-        max_dynamic_size: Optional[str] = None,
+        ds,
+        prediction_file,
 ):
-    ds = FlickrDataset(filename=filename,
-                       image_folder=image_folder,
-                       template_string=template_string,
-                       template_file=template_file,
-                       max_dynamic_size=max_dynamic_size)
+    prediction = json.load(open(prediction_file, 'r', encoding='utf8'))
+    preds = [obj['pred'] for obj in prediction['detail']]
+    targets = [obj['target'] for obj in prediction['detail']]
+
+    assert len(preds) == len(targets) == len(ds), f"{len(preds)}, {len(ds)}"
 
     box_formatter = PlainBoxFormatter()
 
@@ -28,18 +30,14 @@ def format_examples(
         list_of_boxes = box_formatter.extract(string)
         return list_of_boxes
 
-    prediction = json.load(open(prediction_file, 'r', encoding='utf8'))
-    preds = [obj['pred'] for obj in prediction['detail']]
-    targets = [obj['target'] for obj in prediction['detail']]
-
-    assert len(preds) == len(targets) == len(ds), f"{len(preds)}, {len(ds)}"
-
     for idx, (pred, target) in enumerate(zip(preds, targets)):
+        print(idx)
         raw = ds[idx]
         extract_pred = extract_ans(pred)
         extract_target = extract_ans(target)
 
         yield dict(
+            idx=idx,
             raw=raw,
             extract_pred=extract_pred,
             extract_target=extract_target,
@@ -49,20 +47,26 @@ def format_examples(
 
 
 def check_example(example):
+    idx = example['idx']
+
+    image = example['raw']['image']
+    image = expand2square(image)
     colors = ['red', 'green', 'blue', 'yellow', '#533c1b', '#c04851']
     boxes_to_draw = []
     color_to_draw = []
     for idx, boxes in enumerate(example['extract_pred']):
         color = colors[idx % len(colors)]
         for box in boxes:
-            boxes_to_draw.append(box)
+            boxes_to_draw.append(de_norm_box_xyxy(box, w=image.width, h=image.height))
             color_to_draw.append(color)
 
-    from mllm.utils import draw_bounding_boxes, show
-    res = draw_bounding_boxes(image=example['raw']['image'], boxes=boxes_to_draw, colors=color_to_draw, width=4)
-    show(res)
+    try:
+        res = draw_bounding_boxes(image=image, boxes=boxes_to_draw, colors=color_to_draw, width=4)
+        show(res)
+    except:
+        print('error when draw box')
     plt.show()
-    plt.savefig("./tmp.jpg", dpi=300)
+    plt.savefig(f'temp.jpg', dpi=300, bbox_inches='tight')
     plt.close()
 
 
@@ -76,10 +80,17 @@ def main(*args, **kwargs):
 
 
 if __name__ == '__main__':
-    data_file = r'/mnt/lustre/share_data/chenkeqin/VG/pretrain_data/REC/REC_ref4-genome-tiny/val.jsonl'
-    prediction_file = r'/mnt/lustre/share_data/chenkeqin/exp_unify_mllm/eval_rec_llava_origin_5_1/eval_extra_prediction.json'
+    cfg = dict(
+        type='FlickrDataset',
+        filename=r'/mnt/lustre/share_data/chenkeqin/VG/MM_Instruction/flickr30k/test.jsonl',
+        image_folder=r'zz1424:s3://production-public-flickr_image/Flickr_Image/unzip/flickr30k_images/flickr30k_images',
+        template_file=r'/mnt/lustre/share_data/chenkeqin/VG/MM_Instruction/flickr30k_norm01_expand2square/capbox_question_template.json',
+        max_dynamic_size=1,
+    )
+    ds = DATASETS.build(cfg=cfg)
 
+    prediction_file = r'/mnt/lustre/share_data/chenkeqin/exp_unify_mllm/flickr_llava_v1_7b_eval/test_extra_prediction.json'
     main(
-        data_file=data_file,
+        ds=ds,
         prediction_file=prediction_file,
     )
