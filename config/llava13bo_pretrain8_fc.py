4968c4c66618fee780d2201975a97b8421134fdd
chenkq <sy2106101@buaa.edu.cn>
2023-06-16 23:17:31 +0800

    update truncation_config and num_epoch

--- config/llava13bo_pretrain8_fc.py
+++ config/llava13bo_pretrain8_fc.py
@@ -1,9 +1,13 @@
 _base_ = ['_base_/dataset/mix_pretrain_concat8.py', '_base_/model/llava_v1_13bo.py', '_base_/train/llava_fsdp.py']
 
 training_args = dict(
+    num_train_epochs=3,
     output_dir='/mnt/lustre/share_data/chenkeqin/dummy_exp_unify_mllm/{{fileBasenameNoExtension}}',
 )
 
 model_args = dict(
+    conv_args=dict(
+        tokenize_kwargs=dict(truncation_size=1024),
+    ),
     model_name_or_path=r'/mnt/lustre/share_data/chenkeqin/dummy_exp_unify_mllm/llava13bo_pretrain3+concat+recvg+e1',
 )
--- config/llava_pretrain8.py
+++ config/llava_pretrain8.py
@@ -1,5 +1,12 @@
 _base_ = ['_base_/dataset/mix_pretrain_concat8.py', '_base_/model/llava_v1_7b.py', '_base_/train/llava_fsdp.py']
 
 training_args = dict(
+    num_train_epochs=3,
     output_dir='/mnt/lustre/share_data/chenkeqin/dummy_exp_unify_mllm/{{fileBasenameNoExtension}}',
+)
+
+model_args = dict(
+    conv_args=dict(
+        tokenize_kwargs=dict(truncation_size=1024),
+    ),
 )
