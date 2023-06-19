a3e48b2a219c9b35a4ae66976e55ed364c0fa7b8
chenkq <sy2106101@buaa.edu.cn>
2023-06-19 11:49:29 +0800

    GPT4GEN

--- config/llava_finetune_gpt4gen_qbc.py
+++ config/llava_finetune_gpt4gen_qbc.py
@@ -1,15 +1,31 @@
 _base_ = ['_base_/dataset/DEFAULT_TRAIN_DATASET.py', '_base_/model/llava_v1_7b.py', '_base_/train/llava_fsdp.py']
 
 training_args = dict(
-    max_steps=599,
-    save_steps=200,
-    save_total_limit=2,
     output_dir='/mnt/lustre/share_data/chenkeqin/dummy_exp_unify_mllm/{{fileBasenameNoExtension}}',
 )
 
 data_args = dict(
     #
-    train={{_base_.DEFAULT_TRAIN_DATASET.GPT4GEN_QBC}},
+    train=dict(
+        type='InterleaveDateset',
+        cfgs=[
+            {{_base_.DEFAULT_TRAIN_DATASET.flickr}},
+            {{_base_.DEFAULT_TRAIN_DATASET.rec}},
+            {{_base_.DEFAULT_TRAIN_DATASET.reg}},
+            {{_base_.DEFAULT_TRAIN_DATASET.gc}},
+            {{_base_.DEFAULT_TRAIN_DATASET.caption}},
+            dict(
+                type='ConcatDataset',
+                cfgs=[
+                    {{_base_.DEFAULT_TRAIN_DATASET.GPT4GEN_QBC}},
+                    {{_base_.DEFAULT_TRAIN_DATASET.GPT4GEN_RD_QBC}},
+                ],
+            )
+        ],
+        probabilities=[1/6, 1/6, 1/6, 1/6, 1/6, 1/6],
+        seed=None,
+        stopping_strategy='first_exhausted',
+    ),
     validation=None,
     test=None,
 
