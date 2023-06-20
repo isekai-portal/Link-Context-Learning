4f83376074dbed44fac71149e3a57752fd832090
chenkq <sy2106101@buaa.edu.cn>
2023-06-20 16:42:44 +0800

    add flickr test

--- config/_base_/dataset/DEFAULT_TEST_FLICKR_VARIANT.py
+++ config/_base_/dataset/DEFAULT_TEST_FLICKR_VARIANT.py
@@ -1,17 +1,30 @@
 FLICKR_TEST_COMMON_CFG = dict(
     type='FlickrDataset',
     image_folder=r'zz1424:s3://production-public-flickr_image/Flickr_Image/unzip/flickr30k_images/flickr30k_images',
-    template_file=r'{{fileDirname}}/template/flickr30k.json',
     max_dynamic_size=None,
 )
 
 DEFAULT_TEST_FLICKR_VARIANT = dict(
-    FLICKR_EVAL=dict(
+    FLICKR_EVAL_with_box=dict(
         **FLICKR_TEST_COMMON_CFG,
         filename=r'/mnt/lustre/share_data/chenkeqin/mllm_data/pretrain_data/ann/CWB_flickr30k_eval.jsonl',
+        template_file=r'{{fileDirname}}/template/flickr30k.json',
     ),
-    FLICKR_TEST=dict(
+    FLICKR_EVAL_without_box=dict(
+        **FLICKR_TEST_COMMON_CFG,
+        filename=r'/mnt/lustre/share_data/chenkeqin/mllm_data/pretrain_data/ann/CWB_flickr30k_eval.jsonl',
+        template_file=r'{{fileDirname}}/template/image_cap.json',
+    ),
+
+    FLICKR_TEST_with_box=dict(
         **FLICKR_TEST_COMMON_CFG,
         filename=r'/mnt/lustre/share_data/chenkeqin/mllm_data/pretrain_data/ann/CWB_flickr30k_test.jsonl',
+        template_file=r'{{fileDirname}}/template/flickr30k.json',
+    ),
+
+    FLICKR_TEST_without_box=dict(
+        **FLICKR_TEST_COMMON_CFG,
+        filename=r'/mnt/lustre/share_data/chenkeqin/mllm_data/pretrain_data/ann/CWB_flickr30k_test.jsonl',
+        template_file=r'{{fileDirname}}/template/image_cap.json',
     ),
 )
