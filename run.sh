11d8579084a563ee6dce8818ddd910c0c1541f27
fanweichen2 <fanweichen2@sensetime.com>
2023-07-13 15:50:25 +0800

    fix bugs

--- run.sh
+++ run.sh
@@ -89,4 +89,5 @@ sbatch --nodes 1 \
        --comment "wbsR-SC230999.001.02" \
        launcher_intelmpi.sh mllm/pipeline/finetune.py config/icl_debug.py \
        --tf32=False --bf16=False --fp16=True --overwrite_output_dir \
+       --cfg-options model_args.model_name_or_path='/mnt/lustre/share_data/chenkeqin/ckpt/llava_pretrain_final19/checkpoint-40000' \
        --cfg-options data_args.use_icl=False
