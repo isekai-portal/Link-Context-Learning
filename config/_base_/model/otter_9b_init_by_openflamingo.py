f45a99f5ce18c5f2a781f26550901c7b8c80922e
taiyan <taiyan@sensetime.com>
2023-08-29 15:53:58 +0800

    remove extra configs

--- config/_base_/model/otter_9b_init_by_openflamingo.py
+++
@@ -1,7 +0,0 @@
-_base_ = ['./otter_9b.py']
-# openflamingo not train <answer> token, so this version model only support
-# eval on ckpt of finetuned model. not the origin openflamingo weight.
-
-model_args = dict(
-    model_name_or_path="",
-)
--- config/icl_qformer_debug.py
+++
@@ -1,5 +0,0 @@
-_base_ = ['_base_/dataset/mix_icl_prob.py', '_base_/model/llava_v1_7b_qformer.py', '_base_/train/llava_fsdp.py']
-
-training_args = dict(
-    output_dir='/mnt/cache/fanweichen2/Code/unify_mllm/tmp/{{fileBasenameNoExtension}}',
-)
